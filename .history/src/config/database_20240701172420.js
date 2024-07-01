17ff5ddee58601d4fe179782c3e10b442ac78e9d
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2024-07-01 17:31:05 +0700

    Css home page

--- .history/src/config/database_20240701172420.js
+++ .history/src/config/database_20240701172420.js
@@ -2,11 +2,14 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 //create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 module.exports = connection;
